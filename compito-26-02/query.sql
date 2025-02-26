12880ee1b3b039fa43ecf2a8f330488d9ed44a73
camillazicari <camyzicari01@gmail.com>
2025-02-26 14:03:02 +0100

    corretti errori

--- compito-26-02/query.sql
+++ compito-26-02/query.sql
@@ -10,10 +10,10 @@ SELECT COUNT(*) as TotaleClienti FROM Customers;
 SELECT City, COUNT(*) as ClientiLondra FROM Customers WHERE City = 'London' GROUP BY City;
 
 --4--
-SELECT SUM(Freight)  as TotaleTrasporto FROM Orders;
+SELECT AVG(Freight)  as TotaleTrasporto FROM Orders;
 
 --5--
-SELECT CustomerID, SUM(Freight) as TotTrasportoBOTTM FROM Orders WHERE CustomerID = 'BOTTM' GROUP BY CustomerID ;
+SELECT CustomerID, AVG(Freight) as TotTrasportoBOTTM FROM Orders WHERE CustomerID = 'BOTTM' GROUP BY CustomerID ;
 
 --6--
 SELECT CustomerID, SUM(Freight) as TotTrasporto FROM Orders GROUP BY CustomerID;
@@ -25,10 +25,10 @@ SELECT City, COUNT(CustomerID) as TotClienti FROM Customers GROUP BY City;
 SELECT OrderID ,SUM(UnitPrice * Quantity) as Tot FROM [Order Details] GROUP BY OrderID;
 
 --9--
-SELECT OrderID, SUM(UnitPrice * Quantity) as Tot FROM [Order Details] WHERE OrderID = '10248' GROUP BY OrderID;
+SELECT OrderID, SUM(UnitPrice * Quantity) as Tot FROM [Order Details] WHERE OrderID = 10248 GROUP BY OrderID;
 
 --10--
-SELECT CategoryID, SUM(ProductId) as TotaleProdotti FROM Products GROUP BY CategoryID;
+SELECT CategoryID, COUNT(ProductId) as TotaleProdotti FROM Products GROUP BY CategoryID;
 
 --11--
 SELECT ShipCountry ,COUNT(OrderID) as NumeroOrdini FROM Orders GROUP BY ShipCountry;
